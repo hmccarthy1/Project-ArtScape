374fd13f1d970bc15117af0c736768953a3bf6d7
Hunter McCarthy <huntermccarthy56@hotmail.com>
2023-02-08 18:42:24 -0500

    changed api to static call to starry night and the astronauts

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,6 @@
 console.log("Hello, World!");
 
-fetch("https://api.artic.edu/api/v1/artworks/search?q=starrynight").then(function(response) {
+fetch("https://api.artic.edu/api/v1/artworks/search?129884").then(function(response) {
     return response.json();
 }).then(function(data) {
 
